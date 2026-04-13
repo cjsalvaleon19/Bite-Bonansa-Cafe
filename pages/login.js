83da3c40d4f914c364c8a2a6a1a849d1fb99ae78
cjsalvaleon19 <cjsalvaleon19@gmail.com>
2026-04-13 22:39:58 +0800

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import Link from 'next/link';
+import React, { useState } from 'react';
 import { supabase } from '../utils/supabaseClient';
 
 const Login = () => {
